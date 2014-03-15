af71c65b2894518354595be9a6e0e080e588d24f
Robert Garden <robgarden@gmail.com>
2014-03-15 16:28:40 +0200

    Cleaning up Dual Numbers

--- core/src/main/scala/cilib/functions/Continuous.scala
+++ core/src/main/scala/cilib/functions/Continuous.scala
@@ -1,52 +1,52 @@
 package cilib.functions.continuous
 
+import cilib.DualSolution
+import cilib.math.Dual
+import cilib.math.Dual.implicits._
+import cilib.math.Dual.operations._
+
 import spire.implicits._
 import spire.math._
 
-import cilib.Solution
-import cilib.math.Dual
-import cilib.math.DualImplicits._
-
 trait ContinuousFunction {
-	def apply(x: Solution): Dual
+	def apply(x: DualSolution): Dual
 }
 
 object AbsoluteValue extends ContinuousFunction {
-	def apply(x: Solution) = 
-		x.zipWithIndex.map(xi => absD(Dual.one(xi._1, xi._2, x.size))).reduce(_ + _)
+	def apply(x: DualSolution) = x.map(absD(_)).reduce(_ + _)
 }
 
 object Rosenbrock extends ContinuousFunction {
-	def apply(x: Solution) = x.zipWithIndex.sliding(2).map {
-			case Vector(xi, xi1) => {
-				val xid = Dual.one(xi._1, xi._2, x.size)
-				val xi1d = Dual.one(xi1._1, xi1._2, x.size)
-				((-xid + 1) ** 2) + ((xi1d - (xid ** 2)) * 100) ** 2
-			}
+	def apply(x: DualSolution) = x.sliding(2).map {
+			case Vector(xi, xi1) => ((xi1 - xi ** 2) ** 2) * 100 + (xi - 1) ** 2 
 	}.reduce(_ + _)
 }
 
 object Rastrigin extends ContinuousFunction {
-	def apply(x: Solution) = {
-		x.zipWithIndex.map(xi => Dual.one(xi._1, xi._2, x.size)).map { xi =>
+	def apply(x: DualSolution) = x.map { xi =>
 			xi ** 2 - cosD(xi * 2 * pi) * 10
 		}.reduce(_ + _) * (10 + x.size)
+}
+
+object Salomon extends ContinuousFunction {
+	def apply(x: DualSolution) = {
+		val sqrtSquares = sqrtD(Sphere(x))
+		-cosD(sqrtSquares * 2 * pi) + (sqrtSquares * 0.1) + 1
 	}
 }
 
-// object Salomon extends ContinuousFunction {
-// 	def apply(x: Solution): Double = {
-// 		val sqrtSquares = sqrt(Sphere(x))
-// 		-cos(2 * pi * sqrtSquares) + (0.1 * sqrtSquares) + 1
-// 	}
-// }
-
 object Sphere extends ContinuousFunction {
-	// def f(x: Dual) = x ** 2
-	def apply(x: Solution) = 
-		x.zipWithIndex.map(xi => Dual.one(xi._1, xi._2, x.size) ** 2).reduce(_ + _)
+	def apply(x: DualSolution) = x.map(_ ** 2).reduce(_ + _)
 }
 
 // object Step extends ContinuousFunction {
-// 	def apply(x: Solution) = x.map(xi => (floor(xi) + 0.5) ** 2).sum
+// 	def apply(x: DualSolution) = x.map(xi => (floor(xi) + 0.5) ** 2).sum
 // }
+
+object Vincent extends ContinuousFunction {
+	def apply(x: DualSolution) = x.map(xi => sinD(logD(xi) * 10.0)).reduce(_ + _)
+}
+
+object EqualMaxima extends ContinuousFunction {
+	def apply(x: DualSolution) = x.map(xi => sinD(xi * 5 * pi) ** 6).reduce(_ + _)
+}

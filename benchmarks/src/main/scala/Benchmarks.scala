f4df93ab4ef2faa0ef45e1fc3823d2e867a6a975
Robert Garden <robgarden@gmail.com>
2015-07-05 20:50:48 +0200

    Moved Sized types

--- benchmarks/src/main/scala/Benchmarks.scala
+++ benchmarks/src/main/scala/Benchmarks.scala
@@ -3,7 +3,7 @@ package benchmarks
 
 import _root_.scala.Predef.{any2stringadd => _, _}
 
-import scalaz.{Functor,Foldable,Foldable1,Applicative,Monoid,NonEmptyList,Id,OneAnd}
+import scalaz.{Foldable,Foldable1,Functor,Monoid,NonEmptyList}
 import scalaz.syntax.apply._
 import scalaz.syntax.foldable1._
 import scalaz.std.list._
@@ -11,41 +11,13 @@ import scalaz.std.option._
 import scalaz.syntax.std.option._
 
 import spire.math._
-import spire.algebra.{Monoid => _, _}
+import spire.algebra.{Field,IsReal,Order,NRoot,Ring,Signed,Trig}
 import spire.implicits._
+
+import Sized._
 
 object Benchmarks {
 
-  type Sized1[A] = Id.Id[A]
-  type Sized2[A] = (A, A)
-  type Sized3[A] = (A, A, A)
-  type Sized4[A] = (A, A, A, A)
-  type Sized5[A] = (A, A, A, A, A)
-  type Sized6[A] = (A, A, A, A, A, A)
-
-  type Sized1And[F[_], A] = OneAnd[F, A]
-  final case class Sized2And[F[_], A](a: A, b: A, rest: F[A])
-
-  def toSized1[F[_]: Foldable, A](x: F[A]): Option[Sized1[A]] = x.index(0)
-
-  def toSized2[F[_]: Foldable, A](x: F[A]): Option[Sized2[A]] =
-    (x.index(0) |@| x.index(1)) { (_, _) }
-
-  def toSized3[F[_]: Foldable, A](x: F[A]): Option[Sized3[A]] =
-    (x.index(0) |@| x.index(1) |@| x.index(2)) { (_, _, _) }
-
-  def toSized4[F[_]: Foldable, A](x: F[A]): Option[Sized4[A]] =
-    (x.index(0) |@| x.index(1) |@| x.index(2) |@| x.index(3)) { (_, _, _, _) }
-
-  def toSized5[F[_]: Foldable, A](x: F[A]): Option[Sized5[A]] =
-    (x.index(0) |@| x.index(1) |@| x.index(2) |@| x.index(3) |@| x.index(4)) { (_, _, _, _, _) }
-
-  def toSized6[F[_]: Foldable, A](x: F[A]): Option[Sized6[A]] =
-    (x.index(0) |@| x.index(1) |@| x.index(2) |@| x.index(3) |@| x.index(4) |@| x.index(5)) {
-      (_, _, _, _, _, _)
-    }
-
-  // Functions
   def absoluteValue[F[_]: Foldable1, A: Signed: Monoid](x: F[A]) =
     x.foldMap(abs(_))
 

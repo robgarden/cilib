ba3a5565fa06c197fd5dd4b1942d7bbb6c30f482
Robert Garden <robgarden@gmail.com>
2015-11-19 17:12:32 +0100

    Initial attempt at per-iteration measurements

--- core/src/main/scala/cilib/Runner.scala
+++ core/src/main/scala/cilib/Runner.scala
@@ -10,4 +10,17 @@ object Runner {
       alg.run(a).map(_.toList)
     })
   }
+
+  // (Iteration, Swarm) => C
+  type Measure[B,C] = (Int,List[B]) => C
+
+  def repeatWithMeasurement[F[_],A,B,C](n: Int, alg: Kleisli[Step[F,A,?],List[B],Result[B]],
+    collection: RVar[List[B]],
+    measure: Measure[B,C]): Step[F,A,List[C]] = {
+
+    Step.pointR(collection).flatMap(coll =>
+        (1 to n).toStream.foldLeftM[Step[F,A,?],(List[B], List[C])]((coll, List())) { (a, c) =>
+          alg.run(a._1).map(l => (l.toList, a._2 :+ measure(c, l.toList)))
+    }).map(_._2)
+  }
 }
--- example/src/main/scala/cilib/example/OldGBest.scala
+++ example/src/main/scala/cilib/example/OldGBest.scala
@@ -8,21 +8,25 @@ import scalaz.effect.IO.putStrLn
 import scalaz.std.list._
 import spire.implicits._
 
-object OldGBest extends SafeApp {
+object MeasureGBest extends SafeApp {
 
   val sum = Problems.spherical
 
-  // Define a normal GBest PSO and run it for a single iteration
-  val cognitive = Guide.pbest[Mem[List,Double],List,Double]
-  val social = Guide.gbest[Mem[List,Double],List]
+  val cognitive = Guide.pbest[Mem[List,Double],List,Double]; val social = Guide.gbest[Mem[List,Double],List]
 
   val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
 
-  // RVar
-  val swarm = Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(Interval(closed(-5.12),closed(5.12))^30, 20)
+  val swarm = Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(Interval(closed(-5.12),closed(5.12))^30,20)
   val syncGBest = Iteration.sync(gbestPSO)
 
-  // Our IO[Unit] that runs at the end of the world
+  def fitness(n: Int, coll: List[Particle[Mem[List,Double],List,Double]])(implicit M: Memory[Mem[List,Double],List,Double]) = {
+    val fittest = coll.map(e => M._memory.get(e.state))
+        .reduceLeft((a, c) => Fitness.compare(a, c) run Min)
+        .fit.map(_.fold(_.v,_.v))
+
+    (n, fittest)
+  }
+
   override val runc: IO[Unit] =
-    putStrLn(Runner.repeat(1000, syncGBest, swarm).run(Min)(sum).run(RNG.fromTime).toString)
+    putStrLn(Runner.repeatWithMeasurement(1000, syncGBest, swarm, fitness).run(Min)(sum).eval(RNG.fromTime).toString)
 }

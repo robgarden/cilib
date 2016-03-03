244866979487086b28ded5c59efab0a39dbe049d
Robert Garden <robgarden@gmail.com>
2016-03-03 21:47:20 +0100

    PCX Boltzmann Problems

---
+++ example/src/main/scala/cilib/example/PCXBoltzmannProblems.scala
@@ -0,0 +1,84 @@
+package cilib
+package example
+
+import scalaz._
+import Scalaz._
+import scalaz.concurrent.Task
+
+import spire.implicits._
+
+import pl.project13.scala.rainbow._
+
+object PCXBoltzmannProblems {
+
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) = {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
+
+  val dim = 10
+  val problems = Problems.benchmarkSet(dim)
+
+  val s1 = 0.7
+  val s2 = 0.38
+
+  val repeats = 30
+  val iterations = 1000
+
+  val output = "/home/robertgarden/Dropbox/results/problems"
+
+  val strategy = "pcx-boltzmann"
+
+  println()
+  println(s"Running: '${strategy.magenta}':")
+  println(s"Iterations: \t $iterations")
+  println(s"Repeats: \t $repeats")
+  println(s"Problems: \t ${problems.size}")
+  println(s"Dimension: \t $dim")
+  println(s"Total Runs: \t ${repeats * problems.size}".red)
+  println(s"Writing To: \t ${output.green}")
+  println("Starting...".yellow)
+  println()
+
+  def starTask(prob: ProblemDef, seed: Long): Task[Maybe[Double]] = Task {
+    val domain = Interval(closed(prob.l),closed(prob.u))^prob.dim
+
+    val temperatures = ParamHelper.decreasing(1, 100, iterations)
+
+    val algs = temperatures.map { temp =>
+      val cognitive = Guide.pbest[Mem[List,Double],List,Double]
+      val guide = Guide.pcxBoltzmann[Mem[List,Double],List](s1, s2, temp, (c, _) => c)
+      val pcx = Defaults.gbestBounded(0.729844, 1.496180, 1.496180, cognitive, guide, domain.list.toList)
+      Iteration.sync(pcx)
+    }
+
+    val swarm = Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(domain, 20)
+    val finalParticles = Runner.repeatA(algs, swarm).run(Min)(prob.problem).eval(RNG init seed)
+
+    val fitnesses = finalParticles.traverse(e => e.state.b.fit).map(_.map(_.fold(_.v,_.v)))
+
+    println(s"${prob.name} $seed")
+
+    fitnesses.map(_.min)
+  }
+
+  def starLineTask(prob: ProblemDef): Task[String] = {
+    val tasks: Task[List[Maybe[Double]]] = Task.gatherUnordered((0 until repeats).toList.map(i => starTask(prob, i.toLong)))
+    val taskAvg: Task[String] = tasks.map(l => l.sequence.map(_.mkString(",")).getOrElse("Error"))
+    taskAvg.map(avgs => s"$strategy,$iterations,$repeats,${prob.name},$dim,$s1,$s2,$avgs")
+  }
+
+  val problemTasks = Task.suspend(Task.gatherUnordered(problems.map(starLineTask)))
+
+  def writeOut(lines: List[String]) = {
+    printToFile(new java.io.File(s"${output}/${strategy}.txt")) { p =>
+      p.println(s"Strategy,Iterations,Repeat,ProblemClass,Problem,Dimension,s1,s2,avgs")
+      lines.foreach(p.println)
+    }
+  }
+
+  def main(args: Array[String]) = {
+    writeOut(problemTasks.unsafePerformSync)
+    println("Done")
+  }
+}
